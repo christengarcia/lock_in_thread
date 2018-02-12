d839300c35b07faf4e57eba1bb8875a6ddfad484
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-12 20:45:54 +1100

    Initial commit

---
+++ lock_in_thread/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  C# Program to Demonstrate Lock in Thread
+ */
+
+using System;
+using System.Threading;
+
+namespace lock_in_thread
+{
+    class Program
+    {
+        static readonly object _object = new object();
+
+        static void TEST()
+        {
+            lock (_object)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(Environment.TickCount);
+            }
+        }
+    }
+}
